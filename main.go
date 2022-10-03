8c209e8186642eb4c94c2dddce937d62f55182e5
alifakbarr <alifakbar8800@gmail.com>
2022-10-03 09:35:32 +0700

    docs: add Handler - main.go

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello")
+	}
+
 	server := http.Server{
-		Addr: "localhost:8080",
+		Addr:    "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
