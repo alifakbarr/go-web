699717b914b22c3a939e9b2a0107a38ca3784aed
alifakbarr <alifakbar8800@gmail.com>
2022-10-03 09:22:50 +0700

    docs: configure server in port 8080 - main.go

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+)
+
 func main() {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 
 }
